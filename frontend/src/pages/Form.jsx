dfec70de8a41a871ec4c73df642ce64b05dbd64c
ravindu0823 <guestpc87@gmail.com>
2023-06-12 21:41:53 +0530

    Added MDB

--- frontend/src/pages/Form.jsx
+++ frontend/src/pages/Form.jsx
@@ -1,11 +1,67 @@
-import React from 'react';
+import React from "react";
+import {
+  MDBRow,
+  MDBCol,
+  MDBInput,
+  MDBCheckbox,
+  MDBBtn,
+} from "mdb-react-ui-kit";
 
 const Form = () => {
-    return (
-        <div>
-            <h1>Form</h1>
-        </div>
-    );
+  return (
+    <>
+      <div className={"container mt-5"}>
+        <form>
+          <MDBRow className="mb-4">
+            <MDBCol>
+              <MDBInput id="form6Example1" label="First name" />
+            </MDBCol>
+            <MDBCol>
+              <MDBInput id="form6Example2" label="Last name" />
+            </MDBCol>
+          </MDBRow>
+
+          <MDBInput
+            wrapperClass="mb-4"
+            id="form6Example3"
+            label="Company name"
+          />
+          <MDBInput wrapperClass="mb-4" id="form6Example4" label="Address" />
+          <MDBInput
+            wrapperClass="mb-4"
+            type="email"
+            id="form6Example5"
+            label="Email"
+          />
+          <MDBInput
+            wrapperClass="mb-4"
+            type="tel"
+            id="form6Example6"
+            label="Phone"
+          />
+
+          <MDBInput
+            wrapperClass="mb-4"
+            textarea
+            id="form6Example7"
+            rows={4}
+            label="Additional information"
+          />
+
+          <MDBCheckbox
+            wrapperClass="d-flex justify-content-center mb-4"
+            id="form6Example8"
+            label="Create an account?"
+            defaultChecked
+          />
+
+          <MDBBtn className="mb-4" type="submit" block>
+            Place order
+          </MDBBtn>
+        </form>
+      </div>
+    </>
+  );
 };
 
 export default Form;
