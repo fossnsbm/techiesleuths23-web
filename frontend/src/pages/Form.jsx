16dec8fb466dc03418e9d7c7ca2cd06a79cf43df
ravindu0823 <guestpc87@gmail.com>
2023-06-13 01:10:21 +0530

    created API for form.jsx

--- frontend/src/pages/Form.jsx
+++ frontend/src/pages/Form.jsx
@@ -3,16 +3,31 @@ import {
   MDBRow,
   MDBCol,
   MDBInput,
-  MDBCheckbox,
   MDBBtn,
+  MDBValidation,
+  MDBValidationItem,
 } from "mdb-react-ui-kit";
+import axios from "axios";
+import Swal from "sweetalert2";
 
 const Form = () => {
   const [numFields, setNumFields] = useState(0);
+  const [inputValues, setInputValues] = useState([]);
+  const [batch, setBatch] = useState("");
+  const [leaderContact, setLeaderContact] = useState("");
+  const [leaderEmail, setLeaderEmail] = useState("");
+  const [teamName, setTeamName] = useState("");
 
   const handleSelectChange = (event) => {
     const selectedValue = parseInt(event.target.value);
     setNumFields(selectedValue);
+    setInputValues([]);
+  };
+
+  const handleInputChange = (event, index) => {
+    const newInputValues = [...inputValues];
+    newInputValues[index] = event.target.value;
+    setInputValues(newInputValues);
   };
 
   const renderInputFields = () => {
@@ -20,80 +35,133 @@ const Form = () => {
     for (let i = 0; i < numFields; i++) {
       inputFields.push(
         <div key={i}>
-          <MDBInput type="text" className={"mb-4"} label={`Member ${i + 1} Name`} />
+          <MDBInput
+            type="text"
+            required={true}
+            className={"mb-4"}
+            onChange={(event) => handleInputChange(event, i)}
+            label={`Member ${i + 1} Name`}
+          />
         </div>
       );
     }
     return inputFields;
   };
 
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const data = {
+      Batch: batch,
+      "Team Name": teamName,
+      "Member Count": inputValues.length,
+      "Contact Number": leaderContact,
+      Email: leaderEmail,
+      "Member One": inputValues[0] === undefined ? "-" : inputValues[0],
+      "Member Two": inputValues[1] === undefined ? "-" : inputValues[1],
+      "Member Three": inputValues[2] === undefined ? "-" : inputValues[2],
+      "Member Four": inputValues[3] === undefined ? "-" : inputValues[3],
+    };
+
+    axios
+      .post(
+        "https://sheet.best/api/sheets/6f8ea035-5a72-4e41-9ab6-fbb082689809",
+        data
+      )
+      .then((response) => {
+        console.log(response.data);
+        Swal.fire("Submitted!", "Your response has been recorded!", "success");
+      });
+  };
+
   return (
     <>
       <div className={"container mt-5"}>
-        <form>
-          <label className={"mb-2"} htmlFor="numFields">Number Of members</label>
-          <select
-            id="numFields"
-            value={numFields}
-            className={"form-select mb-3"}
-            onChange={handleSelectChange}
-          >
-            <option value="0">Please Select</option>
-            <option value="1">1</option>
-            <option value="2">2</option>
-            <option value="3">3</option>
-            <option value="4">4</option>
-            <option value="5">5</option>
-            {/* Add more options as needed */}
-          </select>
-          {renderInputFields()}
-          <MDBRow className="mb-4">
-            <MDBCol>
-              <MDBInput id="form6Example1" label="First name" />
-            </MDBCol>
-            <MDBCol>
-              <MDBInput id="form6Example2" label="Last name" />
-            </MDBCol>
-          </MDBRow>
+        <MDBValidation>
+          <form>
+            <label className={"mb-2 fw-bold"} htmlFor="batch">
+              Batch
+            </label>
+            <MDBValidationItem
+              feedback={"Please enter you batch name"}
+              invalid={true}
+            >
+              <MDBInput
+                wrapperClass="mb-4"
+                id="batch"
+                required={true}
+                label="Enter Your Batch"
+                value={batch}
+                onChange={(event) => setBatch(event.target.value)}
+              />
+            </MDBValidationItem>
 
-          <MDBInput
-            wrapperClass="mb-4"
-            id="form6Example3"
-            label="Company name"
-          />
-          <MDBInput wrapperClass="mb-4" id="form6Example4" label="Address" />
-          <MDBInput
-            wrapperClass="mb-4"
-            type="email"
-            id="form6Example5"
-            label="Email"
-          />
-          <MDBInput
-            wrapperClass="mb-4"
-            type="tel"
-            id="form6Example6"
-            label="Phone"
-          />
+            <label className={"mb-2 fw-bold"} htmlFor="team_name">
+              Your Team Name
+            </label>
+            <MDBInput
+              wrapperClass="mb-4"
+              id="team_name"
+              required={true}
+              label="Team Name"
+              value={teamName}
+              onChange={(event) => setTeamName(event.target.value)}
+            />
 
-          <MDBInput
-            wrapperClass="mb-4"
-            textarea
-            id="form6Example7"
-            rows={4}
-            label="Additional information"
-          />
+            <label className={"mb-2 fw-bold"} htmlFor="members">
+              Number Of members
+            </label>
+            <select
+              id="members"
+              value={numFields}
+              className={"form-select mb-3"}
+              onChange={handleSelectChange}
+            >
+              <option value="0">Please Select</option>
+              <option value="1">1</option>
+              <option value="2">2</option>
+              <option value="3">3</option>
+              <option value="4">4</option>
+              {/* Add more options as needed */}
+            </select>
+            {renderInputFields()}
+            <MDBRow className="mb-4 mt-5">
+              <MDBCol>
+                <label className={"mb-2 fw-bold"} htmlFor="contact_number">
+                  Leader's Contact Number
+                </label>
+                <MDBInput
+                  id="contact_number"
+                  required={true}
+                  value={leaderContact}
+                  onChange={(event) => setLeaderContact(event.target.value)}
+                  label="Contact Number"
+                />
+              </MDBCol>
+              <MDBCol>
+                <label className={"mb-2 fw-bold"} htmlFor="email">
+                  Leader's Email
+                </label>
+                <MDBInput
+                  value={leaderEmail}
+                  onChange={(event) => setLeaderEmail(event.target.value)}
+                  id="email"
+                  type={"email"}
+                  required={true}
+                  label="Email"
+                />
+              </MDBCol>
+            </MDBRow>
 
-          <MDBCheckbox
-            wrapperClass="d-flex justify-content-center mb-4"
-            id="form6Example8"
-            label="Create an account?"
-            defaultChecked
-          />
-
-          <MDBBtn className="mb-4" type="submit" block>
-            Place order
-          </MDBBtn>
-        </form>
+            <MDBBtn
+              className="mb-4 mt-4"
+              type="submit"
+              onSubmit={(e) => handleSubmit(e)}
+              block
+            >
+              Submit
+            </MDBBtn>
+          </form>
+        </MDBValidation>
       </div>
     </>
   );
